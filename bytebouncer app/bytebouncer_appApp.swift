1bc8a26804c531ef81c5eab79504b9f30a76ac48
ByteBouncer Dev <dev@bytebouncer.app>
2026-03-10 20:32:02 +0100

    Initial Commit

---
+++ bytebouncer app/bytebouncer_appApp.swift
@@ -0,0 +1,17 @@
+//
+//  bytebouncer_appApp.swift
+//  bytebouncer app
+//
+//  Created by AppAgent on 10.03.2026.
+//
+
+import SwiftUI
+
+@main
+struct bytebouncer_appApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ bytebouncer appTests/bytebouncer_appTests.swift
@@ -0,0 +1,16 @@
+//
+//  bytebouncer_appTests.swift
+//  bytebouncer appTests
+//
+//  Created by AppAgent on 10.03.2026.
+//
+
+import Testing
+
+struct bytebouncer_appTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
